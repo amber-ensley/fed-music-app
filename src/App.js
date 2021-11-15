a7320fffc2c7ef8c61da9625176b88ccc0871ab3
Amber Ensley <amber.ensley@quotumtech.com>
2021-11-15 13:35:05 -0500

    added state variables to App component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import ArtistSongComponent from "./ArtistSongComponent";
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [artistName, setArtistName] = useState("Kendrick Lamar");
+  const [artistImgUrl, setArtistImgUrl] = useState("https://images.genius.com/f08637c8cfdeaab4dfbf0631424001ec.640x640x1.jpg");
+  const [artistHits, setArtistHits] = useState([1, 2, 3, 4, 5, 6]);
+
+  const onChangeHandler = (event) => {
+    setSearchTerm(event.target.value)
+  }
 
     return(
         <>
             <header>
                 <h1>playPI</h1>
-                <input className="artist-results-item search" type="text" placeholder="search for artist" />
+                <input 
+                  className="artist-results-item search" 
+                  type="text" 
+                  placeholder="search for artist" 
+                  value={searchTerm}
+                  onChange={onChangeHandler}/>
             </header>
 
             <main className="container">
                 <aside className="item"></aside>
                 <div className="item artist-results">
                     <div className="artist-name-container">
-                    <img className="artist-img" src="https://images.genius.com/f08637c8cfdeaab4dfbf0631424001ec.640x640x1.jpg" alt="Kendrick Lamar" />
-                    <h2 className="artist-name">Kendrick Lamar</h2>
+                    <img className="artist-img" src={artistImgUrl} alt={artistName} />
+                    <h2 className="artist-name">{artistName}</h2>
                 </div>
     
             <div className="artist-hits">
-                {[1, 2, 3, 4, 5, 6].map(item => (
+                {artistHits.map(item => (
                     <ArtistSongComponent
                     key={item} 
                     songTitle="Humble" 
